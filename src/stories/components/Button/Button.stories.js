737f9c3827e08eb3ede676150f509c458f847f01
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2021-02-28 15:06:16 -0500

    [issue-01] install dev tools: - install Storybook - install linter - install node-sass - include sample component - add starting styles

---
+++ src/stories/components/Button/Button.stories.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button } from './Button';
+
+/**
+ * Example Component: Button
+ */
+export default {
+  title: 'Example/Button',
+  component: Button,
+};
+
+const Template = (args) => <Button {...args} />;
+
+// Primary button
+export const Primary = Template.bind({});
+Primary.args = {
+  modifierClasses: '',
+  url: '#',
+  text: 'Primary Button',
+};
+
+// Secondary button
+export const Secondary = Template.bind({});
+Secondary.args = {
+  modifierClasses: 'button--secondary',
+  url: '#',
+  text: 'Secondary Button',
+};
