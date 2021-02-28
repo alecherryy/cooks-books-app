c2dc16b8cfb4ce4ab16f3826cc3484351a6c268a
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2021-02-28 17:26:06 -0500

    [issue-02] Global style: - add brand colors - add fonts - create sample button component

--- src/stories/components/Button/Button.stories.js
+++ src/stories/components/Button/Button.stories.js
@@ -11,18 +11,34 @@ export default {
 
 const Template = (args) => <Button {...args} />;
 
-// Primary button
-export const Primary = Template.bind({});
-Primary.args = {
+// Default button
+export const Default = Template.bind({});
+Default.args = {
   modifierClasses: '',
   url: '#',
-  text: 'Primary Button',
+  text: 'Default Button',
 };
 
-// Secondary button
-export const Secondary = Template.bind({});
-Secondary.args = {
-  modifierClasses: 'button--secondary',
+// Red button
+export const Red = Template.bind({});
+Red.args = {
+  modifierClasses: 'button--red',
   url: '#',
-  text: 'Secondary Button',
+  text: 'Red Button',
+};
+
+// Red button
+export const Blue = Template.bind({});
+Blue.args = {
+  modifierClasses: 'button--blue',
+  url: '#',
+  text: 'Blue Button',
+};
+
+// Purple button
+export const Purple = Template.bind({});
+Purple.args = {
+  modifierClasses: 'button--purple',
+  url: '#',
+  text: 'Purple Button',
 };
