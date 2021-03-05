38f7d549bc7e4a044d0c1f9a42ed8f19756c65ed
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2021-03-05 00:35:56 -0500

    [issue-07] create initial card component

---
+++ src/stories/components/Card/Card.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Card } from './Card';
+
+/**
+ * Example Component: Card
+ */
+export default {
+  title: 'Example/Card',
+  component: Card,
+};
+
+const Template = (args) => <Card {...args} />;
+
+// Default card
+export const Default = Template.bind({});
+Default.args = {
+  isFavorite: false,
+  image: 'https://picsum.photos/200/405',
+  url: '#',
+  title: 'This is a recipe title',
+  description: 'Lorem ipsum dolor sit amet, errem postulant' +
+    ' ei sea, an brute ocurreret sed.',
+  portions: 4,
+  time: 20,
+  rating: 3.5,
+};
