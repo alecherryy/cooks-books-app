aa1c346d984645a7eb839e1daceb6c65117ed0f0
Riley Grant <grantrileyh@gmail.com>
2021-03-28 17:01:06 +0200

    Successfully fetches from API and populates teasers
    Successfully fetches from API and populates teasers

--- src/stories/pages/SearchPage/SearchPage.js
+++ src/stories/pages/SearchPage/SearchPage.js
@@ -1,91 +1,104 @@
 import './styles.scss';
 
-// import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
+import React, { useState } from 'react';
+// import React, { useState, useEffect } from 'react';
+
+// import PropTypes from 'prop-types';
 
 // import { Footer } from '../../components/Footer/Footer';
 import { Teaser } from '../../components/Teaser/Teaser';
-import { SectionTitle } from '../../components/SectionTitle/SectionTitle';
-// import spoonService from '../../../services/spoonacular-service';
+import { Button } from '../../components/Button/Button';
+
+// import { SectionTitle } from '../../components/SectionTitle/SectionTitle';
+import spoonService from '../../../services/spoonacular-service';
 
 import { Constrain } from '../../layouts/Constrain/Constrain';
 import { Grid } from '../../layouts/Grid/Grid';
 
+import img1 from '../../../images/artwork-4.svg';
+
+// import test from '../../../images/artwork-1.svg'
+
 
 /**
- * Component for SearchPage.
+ * Component for Search Page.
  *
  * @component
- * @param {string} test Text to render.
  * @return {object} (
  *  <SearchPage />
  * )
  */
-export const SearchPage = ({ test }) => {
-  // const [listItems, setListItems] = useState(['a', 'b', 'c', 'd']);
-  // const [searchTerms, setSearchTerms] = useState('');
+export const SearchPage = () => {
+  const [resultJSON, setResultJSON] = useState(
+    { totalResults: 0, results: [], number: 0 });
+  // {
+  //   totalResults: 0, results:
+  //     [
+  //       {
+  //         id: 'abc123',
+  //         title: 'DUMMY DATA FOR TEST',
+  //         image: 'https://picsum.photos/id/122/300/350'
+  //       },
+  //     ],
+  // }
 
-  // const updateTermAndSearchApi = () => {
-  //   alert('clicked button');
-  //   setListItems([]);
-  //   // const tempList = [];
-  //   spoonService.findRecipesByKeywords(listItems)
-  //     .then((theRecipes) => alert(theRecipes.length));
-  //   // .then((theRecipes) => theRecipes.map( (recipe) => {
-  //   //   tempList.push( recipe );
-  //   // }));
-  //   // alert(tempList);
-  //   // alert(listItems);
-  // };
+  const [searchTerms, setSearchTerms] = useState('');
+
+  const updateTermAndSearchApi = () => {
+    spoonService.findRecipesByKeywords(searchTerms)
+      .then((theResult) => setResultJSON(theResult));
+  };
 
   return (
     <>
-      {/* <h2>SEARCH_PAGE:_{test}</h2> */}
-      {/* <h4>SEARCH_TERMS:_{searchTerms}</h4> */}
-      {/* <input
-        onChange={(e) => setSearchTerms(e.target.value)}
-        value={searchTerms}></input>
-      <div><button
-        onClick={() => updateTermAndSearchApi()}>Search</button></div>
-      {listItems} */}
-      <Constrain modifierClasses='constrain--narrow'>
-        <h1>Testerino</h1>
-        <SectionTitle
-          imagePath=''
-          title='Search All Recipes'
-          paragraph={`Type almpedit praesent honestatis mea ad,
-          nemore referrentur est ei, usu no omnium partiendo rationibus`} />
-      </Constrain>
+      <Constrain>
 
-      <Grid numColumns={4} >
-        <Teaser />
-        <Teaser />
-        <Teaser />
-        <Teaser />
-      </Grid>
-      {/* <ul>
-        <li>ahhh</li>
-        <li>{listItems}</li>
-        {
-          listItems.map((item) =>
-            <li key={item}>Ahhh{item}</li>,
-          )
-        }
-      </ul> */}
-      <Teaser />
-      {/* TODO: Why does this throw errors for me */}
-      {/* <Footer /> */}
+        {/* TODO: Break this into a component */}
+        <div className='welcome'>
+          <img className='welcome__img' src={img1} alt="img-title"></img>
+          <div className='welcome__title'>
+            <span className='welcome__title-bold'>Search </span>
+            all recipes</div>
+          <p className='welcome__paragraph'>
+            {`Type almpedit praesent honestatis mea ad,
+            nemore referrentur est ei, usu no omnium 
+            partiendo rationibus`}</p>
+        </div>
+
+        <div className='searchbar'>
+          <input
+            onChange={(e) => setSearchTerms(e.target.value)}
+            value={searchTerms}></input>
+          <Button
+            modifierClasses='button--red'
+            isButton={true}
+            text='search'
+            onClick={() => updateTermAndSearchApi()} />
+        </div>
+
+        <div className='results-info'>
+          {`${resultJSON.number} results out of
+           ${resultJSON.totalResults} Recipes`}</div>
+
+        <div className='results-grid'>
+          <Grid numColumns={4} >
+            {resultJSON.results.map((r) => {
+              return (
+                <div key={r.id}>
+                  <Teaser title={r.title} image={r.image} />
+                </div>
+              );
+            })}
+          </Grid>
+        </div>
+
+        {/* MAKE THIS BUTTON CALL API FOR 10 MORE AND APPEND? */}
+        <div className='show-more-btn'>
+          <Button text='Show More' />
+        </div>
+
+      </Constrain>
     </>
   );
-};
-
-SearchPage.propTypes = {
-  /**
-   * SearchPages's text to render
-   */
-  test: PropTypes.string,
-};
-
-SearchPage.defaultProps = {
-  test: 'lorem ipsum',
 };
