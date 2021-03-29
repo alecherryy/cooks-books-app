a7a05d252117c11afdbf67fef60bb84009eb302c
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2021-03-29 18:54:27 -0400

    [issue-51] adjust styles, text size and Teaser image

--- src/stories/pages/Search/Search.js
+++ src/stories/pages/Search/Search.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useParams } from 'react-router';
 
 import { Constrain } from '../../layouts/Constrain/Constrain';
 import { Grid } from '../../layouts/Grid/Grid';
 import { Teaser } from '../../components/Teaser/Teaser';
-import { Button } from '../../components/Button/Button';
+// import { Button } from '../../components/Button/Button';
 import { Form } from '../../components/Form/Form';
 import { FormItem } from '../../components/FormItem/FormItem';
 import { API } from '../../../services/spoonacular-service';
-import artwork from '../../../images/artwork-4.svg';
+import Artwork from '../../../images/artwork-4.svg';
 
 
 /**
@@ -30,6 +30,7 @@ export const Search = () => {
   // listen for changes in url, search based on that
   useEffect(() => {
     setSearchBar(searchTerms);
+
     if (searchTerms) {
       API.findRecipesByKeywords(searchTerms)
         .then((theResults) => setResultJSON(theResults));
@@ -37,61 +38,56 @@ export const Search = () => {
   }, [searchTerms]);
 
   return (
-    <>
-      <Router >
-        <Constrain>
-          <Constrain modifierClasses="constrain--narrow">
-            <img src={artwork} alt="img-title" />
-            <h1 className="text-regular">
-              <span><b>Search </b></span>
-              all recipes
-            </h1>
-            <p>{`Type almpedit praesent honestatis mea ad,
-              nemore referrentur est ei, usu no omnium 
-              partiendo rationibus`}</p>
-            <Form
-              buttonText='Search'
-              handleClick={() => {
-                history.push(`/search/${searchBar}`);
-              }}>
-              <FormItem
-                handleChange={(e) => setSearchBar(e.target.value)}
-                placeholder={'Type something to search for'}
-                value={searchBar} />
-            </Form>
-          </Constrain>
-
-          <br />
-
-          <div className='text-bold'>
-            {`${resultJSON.number} results out of
-           ${resultJSON.totalResults} recipes.`}</div>
-          <br />
-          <Grid numColumns={4} >
-            {resultJSON.results.map((r) => {
-              return (
-                <div key={r.id}>
-                  <Teaser
-                    title={r.title}
-                    image={r.image}
-                    time={r.readyInMinutes}
-                    portions={r.servings}
-                    rating={((r.spoonacularScore + 10) / 20) | 0}
-                    // excerpt={`${r.summary.toString().split('. ')[0]}.`}
-                    excerpt={`${r.title} from ${r.sourceName}`}
-                    url={`/recipes/${r.id}`} />
-                </div>
-              );
-            })}
-          </Grid>
-
-          {/* TODO: MAKE THIS BUTTON CALL API FOR 10 MORE AND APPEND */}
-          <Constrain modifierClasses="constrain--narrow">
-            <Button text='Load More Recipes' />
-          </Constrain>
-
+    <div className="search">
+      <Constrain>
+        <Constrain modifierClasses="constrain--narrow">
+          <img src={Artwork} alt="Decorative Graphics" />
+          <h1>Search <span className="text-regular">
+            all recipes</span>
+          </h1>
+          <p>Type almpedit praesent honestatis mea ad,
+            nemore referrentur est ei, usu no omnium
+            partiendo rationibus</p>
+          <Form
+            buttonText='Search recipes'
+            handleClick={(e) => {
+              e.preventDefault();
+              history.push(`/search/${searchBar}`);
+            }}>
+            <FormItem
+              handleChange={(e) => setSearchBar(e.target.value)}
+              placeholder={'Search for recipes'}
+              value={searchBar} />
+          </Form>
         </Constrain>
-      </Router>
-    </>
+        <p className='text-bold'>{resultJSON.number} results out of
+          {' '}{resultJSON.totalResults} recipes</p>
+        <br />
+        <Grid numColumns={4} >
+          {resultJSON.results.map((r) => {
+            const ratings = r.spoonacularScore ?
+              (r.spoonacularScore / 100) * 5 :
+              null;
+            return (
+              <div key={r.id}>
+                <Teaser
+                  title={r.title}
+                  image={r.image}
+                  time={r.readyInMinutes}
+                  portions={r.servings}
+                  rating={Math.round(ratings * 10) / 10}
+                  excerpt={`${r.summary.toString().split('. ')[0]}.`}
+                  // excerpt={`${r.title} from ${r.sourceName}`}
+                  url={`/recipes/${r.id}`} />
+              </div>
+            );
+          })}
+        </Grid>
+        {/* TODO: MAKE THIS BUTTON CALL API FOR 10 MORE AND APPEND */}
+        {/* <Constrain modifierClasses="constrain--narrow">
+          <Button text='Load More Recipes' />
+        </Constrain> */}
+      </Constrain>
+    </div>
   );
 };
