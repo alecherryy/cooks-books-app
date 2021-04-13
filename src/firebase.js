1c2ab864ab13a855cbc8fb0c6f931f861a984a38
Riley Grant <grantrileyh@gmail.com>
2021-04-13 01:15:40 -0400

    [issue-53] Add user login (#56)
    * [issue-53] add user login
    
    * [issue-53] add small fixes
    
    Co-authored-by: Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 // Made with guidance from :
 // https://www.youtube.com/watch?v=cFgoSrOui2M
@@ -8,16 +9,17 @@ import firebase from 'firebase';
  * to our firebase project.
  */
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DB_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const fire = firebase.initializeApp(firebaseConfig);
-export default fire;
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
